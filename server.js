49ce14b6af2764f0ce81404f8b9bc92f96749bef
Kishore Kumar <kishorekumarmuthu0906@gmail.com>
2025-02-01 13:11:29 +0530

    kishore commit 1

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const User = require("./models/User"); // Import the User model
 const app = express();
 const port = 5000;
+const mongoose = require("mongoose");
 
 // Middleware
 app.use(
@@ -15,16 +16,35 @@ app.use(
 );
 app.use(bodyParser.json());
 
+mongoose
+  .connect("mongodb://localhost:27017/awstest", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB:", err));
+
 // Route to handle form data
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.post("/submit", (req, res) => {
+app.post("/submit", async (req, res) => {
   const { field1, field2, field3, field4 } = req.body;
-  console.log("Received data:", { field1, field2, field3, field4 });
-  res.json({ message: "Data received successfully!" });
+  console.log(req.body);
+  try {
+    const newUser = new User({ field1, field2, field3, field4 });
+    await newUser.save();
+    res
+      .status(200)
+      .json({ message: "User data saved successfully!", data: newUser });
+  } catch (err) {
+    console.error("Error saving user data:", err);
+    res.status(500).json({ error: "Server error" });
+  }
+  // console.log("Received data:", { field1, field2, field3, field4 });
+  // res.json({ message: "Data received successfully!" });
 });
 
 // Start the server
